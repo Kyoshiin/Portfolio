60ffb59c9694c69c12c4911370ba74d94efa92f8
Pratyay <pratyayroy18@gmail.com>
2023-07-02 13:48:51 +0530

    UI changes

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -7,56 +7,58 @@ import { HiArrowDown } from "react-icons/hi";
 const HeroSection = () => {
   return (
     <section id="home">
-      <div
-        className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 
-      my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left"
-      >
-        <div className="md:mt-2 md:w-1/2">
-          <Image
-            src="/img.png"
-            alt="Profile Picture"
-            width={325}
-            height={325}
-            className="rounded-full shadow-2xl"
-          />
-        </div>
+      <div className="md:pb-36">
+        <div
+          className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2
+      py-16 my-10 sm:py-32 md:pt-48 md:pb-28 md:flex-row md:space-x-4 md:text-left"
+        >
+          <div className="md:mt-2 md:w-1/2">
+            <Image
+              src="/img.png"
+              alt="Profile Picture"
+              width={325}
+              height={325}
+              className="rounded-full shadow-2xl"
+            />
+          </div>
 
-        <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-6xl">
-            Hi, I&#39;m Pratyay!
-          </h1>
-          <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I&#39;m a&nbsp;
-            <span className="font-semibold text-teal-600">
-              Software Engineer&nbsp;
-            </span>
-            based in India. Working towards creating software that makes life
-            easier and more meaningful.
-          </p>
+          <div className="md:mt-2 md:w-3/5 md:p-4">
+            <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-6xl">
+              Hi, I&#39;m Pratyay!
+            </h1>
+            <p className="text-lg mt-4 mb-6 md:text-2xl">
+              I&#39;m a&nbsp;
+              <span className="font-semibold text-teal-600">
+                Software Engineer&nbsp;
+              </span>
+              based in India. Working towards creating software that makes life
+              easier and more meaningful.
+            </p>
+            <Link
+              to="projects"
+              className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              Projects
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-row items-center text-center justify-center ">
           <Link
-            to="projects"
-            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
+            to="about"
             activeClass="active"
             spy={true}
             smooth={true}
             offset={-100}
             duration={500}
           >
-            Projects
+            <HiArrowDown size={35} className="animate-bounce" />
           </Link>
         </div>
-      </div>
-      <div className="flex flex-row items-center text-center justify-center ">
-        <Link
-          to="about"
-          activeClass="active"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
-          <HiArrowDown size={35} className="animate-bounce" />
-        </Link>
       </div>
     </section>
   );
--- src/app/components/ProjectSection.tsx
+++ src/app/components/ProjectSection.tsx
@@ -26,53 +26,57 @@ const projects = [
 const ProjectsSection = () => {
   return (
     <section id="projects">
-      <h1 className="my-10 text-center font-bold text-4xl">
-        Projects
-        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
-      </h1>
+      <div className="my-12 pb-12 md:pt-16 md:pb-36">
+        <h1 className="my-10 text-center font-bold text-4xl">
+          Projects
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
 
-      <div className="flex flex-col space-y-28">
-        {projects.map((project, ind) => {
-          return (
-            <div key={ind}>
-              <SlideUp offset="-300px 0px -300px 0px">
-                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
-                  <div className=" md:w-1/2">
-                    <Link href={project.link}>
-                      <Image
-                        src={project.image}
-                        alt=""
-                        width={1000}
-                        height={1000}
-                        className="rounded-xl shadow-xl hover:opacity-70"
-                      />
-                    </Link>
-                  </div>
-                  <div className="mt-8 md:w-1/2">
-                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
-                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
-                      {project.description}
-                    </p>
-                    <div className="flex flex-row align-bottom space-x-4">
-                      <Link href={project.github} target="_blank">
-                        <BsGithub
-                          size={30}
-                          className="hover:-translate-y-1 transition-transform cursor-pointer"
-                        />
-                      </Link>
-                      <Link href={project.link} target="_blank">
-                        <BsArrowUpRightSquare
-                          size={30}
-                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+        <div className="flex flex-col space-y-28">
+          {projects.map((project, ind) => {
+            return (
+              <div key={ind}>
+                <SlideUp offset="-300px 0px -300px 0px">
+                  <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                    <div className=" md:w-1/2">
+                      <Link href={project.link}>
+                        <Image
+                          src={project.image}
+                          alt=""
+                          width={1000}
+                          height={1000}
+                          className="rounded-xl shadow-xl hover:opacity-70"
                         />
                       </Link>
                     </div>
+                    <div className="mt-8 md:w-1/2">
+                      <h1 className="text-4xl font-bold mb-6">
+                        {project.name}
+                      </h1>
+                      <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                        {project.description}
+                      </p>
+                      <div className="flex flex-row align-bottom space-x-4">
+                        <Link href={project.github} target="_blank">
+                          <BsGithub
+                            size={30}
+                            className="hover:-translate-y-1 transition-transform cursor-pointer"
+                          />
+                        </Link>
+                        <Link href={project.link} target="_blank">
+                          <BsArrowUpRightSquare
+                            size={30}
+                            className="hover:-translate-y-1 transition-transform cursor-pointer"
+                          />
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </SlideUp>
-            </div>
-          );
-        })}
+                </SlideUp>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
